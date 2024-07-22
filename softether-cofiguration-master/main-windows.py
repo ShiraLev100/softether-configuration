1e23e18c82357a758559a5c2dd4ac91ea18a6048
ShiraLev100 <Shira.Levinson111@gmail.com>
2024-07-22 11:01:22 +0300

    this is windows version

--- softether-cofiguration-master/main-windows.py
+++ softether-cofiguration-master/main-windows.py
@@ -9,7 +9,6 @@ import ctypes
 import sys
 import subprocess
 #from ttkthemes import ThemedStyle
-import argparse
 
 # Add logging configuration
 import logging
@@ -225,16 +224,13 @@ class SMBClientGUI:
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="SMB Client GUI with command-line options")
-    parser.add_argument('--username', type=str, help="Username for SMB client")
-    parser.add_argument('--password', type=str, help="Password for SMB client")
-    args = parser.parse_args()
-
     if ctypes.windll.shell32.IsUserAnAdmin():
         print("Already running as admin.")
         root = tk.Tk()
-        app = SMBClientGUI(root, args.username, args.password)
+        app = SMBClientGUI(root)
         root.mainloop()
+        input("Press Enter to exit...")
+
     else:
         print("Not running as admin. Attempting to elevate privileges.")
         ctypes.windll.shell32.ShellExecuteW(None, 'runas', sys.executable, __file__, None, 1)
